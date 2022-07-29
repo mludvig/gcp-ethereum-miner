7634e0691803dadd9749319e323e72ee1d712002
Michael Ludvig <mludvig@logix.net.nz>
2022-07-30 11:25:02 +1200

    Create T4 IGM in all regions

--- compute.tf
+++ compute.tf
@@ -9,13 +9,37 @@ data "template_file" "startup_script" {
   }
 }
 
-resource "google_compute_instance" "gpu_instance" {
-  name         = "tf-instance"
+locals {
+  regions_t4 = [
+    "asia-east1",
+    "asia-northeast1",
+    "asia-northeast3",
+    "asia-south1",
+    "asia-southeast1",
+    "asia-southeast2",
+    "australia-southeast1",
+    "europe-west1",
+    "europe-west2",
+    "europe-west3",
+    "europe-west4",
+    "northamerica-northeast1",
+    "southamerica-east1",
+    "us-central1",
+    "us-east1",
+    "us-east4",
+    "us-west1",
+    "us-west2",
+    "us-west4",
+  ]
+}
+
+resource "google_compute_instance_template" "gpu_instance_template" {
+  name         = "gpu-template"
   machine_type = "n1-standard-1"
-  guest_accelerator = [{
+  guest_accelerator {
     type  = "nvidia-tesla-t4"
-    count = 1
-  }]
+    count = 2
+  }
   scheduling {
     preemptible                 = true
     provisioning_model          = "SPOT"
@@ -25,10 +49,10 @@ resource "google_compute_instance" "gpu_instance" {
 
   metadata_startup_script = data.template_file.startup_script.rendered
 
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.gpu_image.self_link
-    }
+  disk {
+    source_image = data.google_compute_image.gpu_image.self_link
+    auto_delete  = true
+    boot         = true
   }
 
   network_interface {
@@ -36,4 +60,17 @@ resource "google_compute_instance" "gpu_instance" {
     access_config {
     }
   }
+}
+
+resource "google_compute_region_instance_group_manager" "gpu" {
+  for_each = toset(local.regions_t4)
+  region = each.value
+  name = "gpu-igm-${each.value}"
+  base_instance_name = "gpu"
+  distribution_policy_target_shape = "ANY"
+
+  version {
+    instance_template  = google_compute_instance_template.gpu_instance_template.id
+  }
+  target_size = 16
 }
